fef1aca0276f8eaa02d3af38429e0ed52273cdd4
Frobenius42 <adrien.marcenat@orange.fr>
2016-11-26 20:01:53 +0100

    levelGenerator can read a json

---
+++ scripts/LevelGenerator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+using System.IO;
+
+public class LevelGenerator : MonoBehaviour {
+
+	private static LevelGenerator instance; // Singleton
+	public string fileName;
+
+	private string levelConfig;
+
+	void Awake() {
+		if (instance == null) {
+			DontDestroyOnLoad (gameObject);
+			instance = this;
+		} else if (instance != this)
+			Destroy (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		levelConfig = readJSON ();
+		generate ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private string readJSON()
+	{
+		StreamReader sr = new StreamReader (Application.dataPath + "/friend-racers/config/" + fileName);
+		string content = sr.ReadToEnd ();
+		sr.Close ();
+
+		return content;
+	}
+
+	private void generate()
+	{
+		JSONNode json = JSON.Parse (levelConfig);
+
+		print (json ["test"].Value);
+	}
+}
